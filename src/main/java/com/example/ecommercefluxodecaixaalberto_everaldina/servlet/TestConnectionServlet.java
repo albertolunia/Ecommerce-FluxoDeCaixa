6d3c13268541a31412942437eef73af3f9239829
albertolunia <albertolunia@gmail.com>
2024-11-03 20:37:54 -0300

    Conexão com o banco de dados bem sucedida

--- src/main/java/com/example/ecommercefluxodecaixaalberto_everaldina/servlet/TestConnectionServlet.java
+++ src/main/java/com/example/ecommercefluxodecaixaalberto_everaldina/servlet/TestConnectionServlet.java
@@ -2,10 +2,10 @@ package com.example.ecommercefluxodecaixaalberto_everaldina.servlet;
 
 import com.example.ecommercefluxodecaixaalberto_everaldina.db.DatabaseConnection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
